0960defb6370635051c098eff053d81ec75eae45
Alex Rodionov <p0deje@gmail.com>
2014-05-16 16:56:14 +0700

    Remove monkey-patch to Vagrant
    See mitchellh/vagrant#3670
    Fixed in Vagrant 1.6.1

--- lib/vagrant-exec.rb
+++ lib/vagrant-exec.rb
@@ -2,13 +2,3 @@ require 'vagrant'
 
 require 'vagrant-exec/plugin'
 require 'vagrant-exec/version'
-
-
-# HACK: make vagrant-exec compatible with vagrant 1.6
-# remove when https://github.com/mitchellh/vagrant/pull/3670 is merged
-class Vagrant::Config::V2::Root
-  def exec
-    method_missing(:exec)
-  end
-end
-
